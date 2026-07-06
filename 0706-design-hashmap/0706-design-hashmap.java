d5fe74ee2b9b10651200930d79a8362dd4b8c0d7
thiriveni2027 <thirivenithiriveni15@gmail.com>
2026-07-06 19:26:41 +0530

    Time: 40 ms (17.53%), Space: 57.5 MB (35.71%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,24 @@
+class MyHashMap {
+
+    int[] map;
+
+    public MyHashMap() {
+        map = new int[1000001];
+
+        for (int i = 0; i < map.length; i++) {
+            map[i] = -1;
+        }
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
